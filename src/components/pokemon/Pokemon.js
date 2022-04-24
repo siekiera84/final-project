290e859c6e500ee0d184508f008ba4fc1bb56986
siekiera84 <kowalczyk.hubert.t@gmail.com>
2022-04-24 23:09:05 +0200

    types-colors

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -1,6 +1,27 @@
 import React, {Component} from 'react';
 import axios from 'axios'
 
+const TYPE_COLORS = {
+    bug: 'B1C12E',
+    dark: '4F3A2D',
+    dragon: '755EDF',
+    electric: 'FCBC17',
+    fairy: 'F4B1F4',
+    fighting: '823551D',
+    fire: 'E73B0C',
+    flying: 'A3B3F7',
+    ghost: '6060B2',
+    grass: '74C236',
+    ground: 'D3B357',
+    ice: 'A3E7FD',
+    normal: 'C8C4BC',
+    poison: '934594',
+    psychic: 'ED4882',
+    rock: 'B9A156',
+    steel: 'B5B5C3',
+    water: '3295F6'
+};
+
 export default class Pokemon extends Component {
     state = {
         name: '',
@@ -30,7 +51,7 @@ export default class Pokemon extends Component {
         const {pokemonIndex} = this.props.match.params;
 
         //url z informacjami pokemonów
-        const pokemonUrl = `https://pokeapi.com/api/v2/pokemon/${pokemonIndex}`
+        const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`
         const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`
 
         //zbieranie informacji o pokemonach
@@ -142,9 +163,18 @@ export default class Pokemon extends Component {
                                 <h5>{this.state.pokemonIndex}</h5>
                             </div>
                             <div className="col-7">
-                                <div className="float-right">
+                                <div className="float-end">
                                     {this.state.types.map(type=>(
-                                        <span key="type" className="badge badge-pill mr-1">{type}</span>
+                                        <span key={type}
+                                              className="badge badge-primary badge-pill mr-1"
+                                              style={{backgroundColor: `#${TYPE_COLORS[type]}`,color: 'white'}}
+                                        >
+                                            {type
+                                            .toLowerCase()
+                                            .split(' ')
+                                            .map(s => s.charAt(0).toUpperCase() +s.substring(1))
+                                            .join(' ')}
+                                        </span>
                                     ))}
                                 </div>
                             </div>
