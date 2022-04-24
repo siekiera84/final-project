e17e98eda527e7ce1dbde47127360367cc2772bd
siekiera84 <kowalczyk.hubert.t@gmail.com>
2022-04-24 19:38:18 +0200

    core-website

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, {useState, useEffect, Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import NavBar from "./components/layout/NavBar";
+import Dashboard from "./components/layout/Dashboard";
+
+import backgroundImage from "./components/pokemon/pattern.png";
 
 
 class App extends Component{
     render(){
         return(
-            <div className="App">
-                <NavBar/>
+            <div className="App" style ={{background: `url(${backgroundImage})`}}>
+                <NavBar/><div className="container"><Dashboard></Dashboard></div>
             </div>
         )
     }
